e0b833e0d5ff91e85f40041dd4373643b0ee9043
StephenChips <tcw6991@gmaili.com>
2023-12-08 06:50:27 +0800

    initialize the project, setup vcpkg and googletest, draft the program's interface.

---
+++ parser-generator/README.md
@@ -0,0 +1,3 @@
+# A toy parser generator
+
+This parser generator implements LL(1), LR(1) and LALR parsing algorithm, and allow you to pick any one of those when you create a new instance.
---
+++ parser-generator/parser-generator/parser-generator.cpp
@@ -0,0 +1,71 @@
+﻿// parser-generator.cpp : Defines the entry point for the application.
+//
+#include <memory>
+#include <map>
+#include <type_traits>
+#include <functional>
+#include <fmt/core.h>
+
+struct ParseResult {};
+
+template<typename ValueType>
+using OnParseCallback = std::function<ValueType(std::vector<ParseResult>)>;
+
+
+template <typename ValueType>
+class NonTerminator {
+    OnParseCallback<ValueType> whenParsedSuccessfully;
+public:
+    NonTerminator& produce(const char* s, OnParseCallback<ValueType>&& lambda) {
+        return *this;
+    }
+
+    NonTerminator& produce(const char* s) {
+        return *this;
+    }
+};
+
+
+template <typename ValueType>
+class Terminator {
+    OnParseCallback<ValueType> whenParsedSuccessfully;
+public:
+    void matches(const char* s) {
+
+    }
+
+    void matches(const char* s, OnParseCallback<ValueType>&& lambda) {
+
+    }
+};
+
+template<typename ValueType>
+class Grammar {
+public:
+    NonTerminator<ValueType>& rule(const char* s) {
+
+    }
+
+    Terminator<ValueType>& token(const char* s, const char* regex) {
+
+    }
+
+    Terminator<ValueType>& token(const char* s, const char* regex, OnParseCallback<ValueType>&& lambda) {}
+};
+
+template <typename ValueType>
+class Parser {
+public:
+    virtual ValueType run() = 0;
+};
+
+template <typename ValueType>
+class LL1Parser : public Parser<ValueType> {
+public:
+    LL1Parser(const Grammar<ValueType>& grammar) {
+
+    }
+    virtual ValueType run() override {
+
+    }
+};
